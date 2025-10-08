7ac84980b3438f4bc5d4c28ccf96da7ba8cf5528
GurikaK77 <223002836+GurikaK77@users.noreply.github.com>
2025-10-08 22:53:13 +0400

    martorqa

--- sw.js
+++ sw.js
@@ -1,14 +1,26 @@
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('states-cache').then(cache => {
+    caches.open('states-cache-v6').then(cache => {
       return cache.addAll([
         './',
         './index.html'
-       
       ]);
     })
   );
 });
+
+// ძველი ქეშების წაშლა
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== 'states-cache-v6')
+            .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
